09bc5ab2e92cda1ad667e37b3765a589a4d360ce
Richard Korn <richard.korn@bearingpoint.com>
2023-01-30 16:11:27 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-angular-project-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'nx-angular-project';
+}
